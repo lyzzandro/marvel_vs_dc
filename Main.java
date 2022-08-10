a0b7442dfc0cec28970329e140d4ab90e80d7e64
lyzzandro <lyzzandro91@gmail.com>
2022-08-10 11:22:44 -0300

    configurando main

--- Main.java
+++ Main.java
@@ -1,3 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Scanner heroi = new Scanner(System.in);
+        int numeroHeroi;
+
+        System.out.println(
+                "---------------------------------------\n Escolha algum dos herois listados e digite o numero correspondente:");
+        System.out.println(
+                "---------------------------------------\n Batman - 1\n Homem Aranha - 2\n Homem de Ferro - 3\n Mulher Maravilha - 4\n Thor - 5\n Superman - 6\n Hulk - 7\n Flash - 8\n ---------------------------------------\n");
+
+        System.out.printf("Informe um numero:\n");
+        numeroHeroi = heroi.nextInt();
+
+        switch (numeroHeroi) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+
+            default:
+                System.out.println("Numero informado invalido");
+                break;
+        }
+
+    }
+
 }
---
+++ generics/Batalha.java
@@ -0,0 +1,9 @@
+package generics;
+
+public class Batalha {
+
+    public static void batalhar(Object heroi1, Object heroi2) {
+
+
+    }
+}
